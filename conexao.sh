060ecaeda0d366f2a8e4321c46df0e5839b584e5
GunGZN <86140154+GunGZN@users.noreply.github.com>
2021-09-09 16:22:26 +0700

    Update conexao.sh

--- conexao.sh
+++ conexao.sh
@@ -1191,9 +1191,9 @@ exit 0' >$RCLOCAL
 				proto $PROTOCOL
 				sndbuf 0
 				rcvbuf 0
-				remote 127.0.0.1 2222
-				route $IP 255.255.255.255 net_gateway
-				#MODO SLOWDNS, UTILIZE O APP OPENVPN PARA ANDROID, SELECIONE O TERMUX EM APLICAÇÕES PERMITIDAS
+				remote $IP $porta
+				http-proxy $IP 8080
+				http-proxy-option CUSTOM-HEADER Host www.opensignal.com
 				resolv-retry 5
 				nobind
 				persist-key
